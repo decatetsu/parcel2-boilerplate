81111b84ca173fb3164afe2f7f5dce585bbb50b2
nekovalue <nekovalue@gmail.com>
2021-11-15 14:15:50 +0200

    Initial commit

---
+++ src/js/App.jsx
@@ -0,0 +1,12 @@
+import butt from "../assets/content/babel.jpg";
+
+function App() {
+    return (
+        <div>
+            <h1>Hello, my darling!</h1>
+            <img src={butt} alt="butt" width={"400px"}/>
+        </div>
+    );
+}
+
+export default App;
